1cbe4630e054be134e43747e73fa61d711a5e62f
Miftaul_Islam_Ariyan <miftaulislam005@gmail.com>
2025-01-04 08:14:22 +0600

    Easy-Array-004:Remove Element problem solved

---
+++ Easy/array/Remove Element/src/Main.java
@@ -0,0 +1,25 @@
+public class Main{
+    public int RemoveElement(int[] nums, int val){
+        int n = nums.length;
+        int k = 0;
+
+        if (n == 0) return 0;
+
+        if(n == 1 && val == nums[0]) return 0;
+        for (int i = 0; i < n; i++) {
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+    return k;
+    }
+
+    public static void main (String[] args){
+        int[] nums = {3,2,2,3};
+        int val = 3;
+        Main maininstance = new Main();
+        System.out.print(maininstance.RemoveElement(nums, val));
+//        maininstance.RemoveElement(nums, val);
+    }
+}
